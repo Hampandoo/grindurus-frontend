ed7b2ffd9f8e71b3b7245c6f14e00e6f4329cbe6
Mykyta <Mykyta.Samokhval@securecrowds.com.ua>
2025-03-20 13:30:56 +0200

    [dev]: Change mint pool logic, added loaders, changed styles

--- src/components/Dashboard/MintPoolNFT/MintPoolNFT.js
+++ src/components/Dashboard/MintPoolNFT/MintPoolNFT.js
@@ -4,24 +4,72 @@ import {ethers} from 'ethers';
 import { Select, MenuItem, FormControl, TextField, Button } from "@mui/material";
 import './MintPoolNFT.css';
 import { useContractService } from '../../../context/ContractContext';
+import { useAppKitAccount } from "@reown/appkit/react"
 
 
 function MintPoolNFT({ networkConfig }) {
-  const { signer } = useContractService();
+  const { provider } = useContractService();
+  const { address } = useAppKitAccount();
 
   const [isApproved, setIsApproved] = useState(false);
+  const [allowance, setAllowance] = useState(0);
 
   const [selectedStrategyId, setSelectedStrategyId] = useState(0);
   const [selectedQuoteTokenId, setQuoteTokenId] = useState(0);
   const [selectedBaseTokenId, setBaseTokenId] = useState(0);
   const [quoteTokenAmount, setQuoteTokenAmount] = useState('');
 
-  useEffect(() => {
+  // Loading button states
+  const [waitApproving, setWaitApproving] = useState(false);
+  const [waitMint, setWaitMint] = useState(false);
 
-  }, [networkConfig]);
+  // Provider added as dependency for checking allowance after user connect wallet
+  useEffect(() => {
+    checkAllowance();
+  }, [networkConfig, selectedQuoteTokenId, provider]);
+  useEffect(() => {
+    if (allowance > 0 && quoteTokenAmount <= allowance) {
+      setIsApproved(true);
+    } else if (allowance > 0) {
+      setIsApproved(false);
+    } else {
+      setIsApproved(false);
+    }
+  }, [quoteTokenAmount]);
+
+  const checkAllowance = async () => {
+    try {
+      if (!provider) {
+        return;
+      }
+        const signer = await provider.getSigner();
+        const userAddress = signer.getAddress();
+        const spenderAddress = networkConfig.poolsnft;
+
+        const quoteTokenInfo = networkConfig.quoteTokens[selectedQuoteTokenId];
+
+        const quoteToken = new ethers.Contract(
+            quoteTokenInfo.address,
+            ["function allowance(address owner, address spender) external view returns (uint256)"],
+            signer
+        );
+
+        const allowanceRaw = await quoteToken.allowance(userAddress, spenderAddress);
+        const allowanceFormatted = ethers.formatUnits(allowanceRaw, quoteTokenInfo.decimals);
+        setAllowance(allowanceFormatted);
+    } catch (error) {
+        console.error("Error checking allowance:", error);
+        // alert("Failed to check token allowance.");
+    }
+} ;
 
   const handleMaxDepositQuoteToken = async () => {
     try {
+      if (!provider) {
+        return;
+      }
+      setWaitApproving(true);
+      const signer = await provider.getSigner();
       const quoteTokenInfo = networkConfig.quoteTokens[selectedQuoteTokenId];
       const quoteToken = new ethers.Contract(
         quoteTokenInfo.address,
@@ -35,16 +83,17 @@ function MintPoolNFT({ networkConfig }) {
       setQuoteTokenAmount(balance)
     } catch {
       alert("Failed to fetch balance");
+    } finally {
+      setWaitApproving(false);
     }
   }
 
   const handleApprove = async () => {
-    if (!window.ethereum) {
-      alert("MetaMask is not installed.");
-      return;
-    }
     try {
-      const provider = new ethers.BrowserProvider(window.ethereum);
+      if (!provider) {
+        return;
+      }
+      setWaitApproving(true);
       const signer = await provider.getSigner();
 
       const quoteTokenInfo = networkConfig.quoteTokens[selectedQuoteTokenId];
@@ -63,22 +112,21 @@ function MintPoolNFT({ networkConfig }) {
       const tx = await quoteToken.approve(spenderAddress, amount);
 
       await tx.wait();
-
       setIsApproved(true);
 
     } catch (error) {
       alert("Failed to approve tokens.");
+    } finally {
+      setWaitApproving(false);
     }
-
   };
 
   const handleMint = async () => {
-    if (!window.ethereum) {
-      alert("MetaMask is not installed.");
-      return;
-    }
     try {
-      const provider = new ethers.BrowserProvider(window.ethereum);
+      if (!provider) {
+        return;
+      }
+      setWaitMint(true);
       const signer = await provider.getSigner();
 
       const poolsNFTAddress = networkConfig.poolsnft;
@@ -106,6 +154,8 @@ function MintPoolNFT({ networkConfig }) {
 
     } catch (error) {
       console.error("Error during approve:", error);
+    } finally {
+      setWaitMint(false);
     }
   };
 
@@ -223,37 +273,56 @@ function MintPoolNFT({ networkConfig }) {
         {!isApproved ? (
           <Button
             className="approve-button"
-            variant="contained"
+            variant={waitApproving ? 'variant' : 'contained'}
             disabled={quoteTokenAmount <= 0}
             sx={{
               borderRadius: "8px",
               fontWeight: 700,
               minWidth: "unset",
-              backgroundColor: "#f7e1fc",
+              backgroundColor: waitApproving ? "transparent" : "#f7e1fc",
+              borderColor: waitApproving ? "#f7e1fc" : "transparent",
+              borderWidth: waitApproving ? "2px" : "0",
+              "&.Mui-disabled": {
+                backgroundColor: "rgba(1,1,1,0)",
+                borderStyle: "solid",
+                borderColor: quoteTokenAmount <= 0 ? "rgba(51, 51, 51, 0.1)" : "transparent",
+                borderWidth: quoteTokenAmount <= 0 ? "2px" : "0",
+              },
+              borderStyle: "solid",
               color: "#c556db",
               textTransform: "none",
               fontSize: "20px",
               lineHeight: 1,
               height: "42px"
             }}
+            loading={waitApproving}
             onClick={handleApprove}
           >Approve Quote Token</Button>
         ) : (
           <Button
             className="mint-button"
-            variant="contained"
-            disabled={quoteTokenAmount <= 0}
+            variant={waitMint ? 'variant' : 'contained'}
+            disabled={quoteTokenAmount <= 0 || allowance < quoteTokenAmount}
             sx={{
               borderRadius: "8px",
               fontWeight: 700,
               minWidth: "unset",
-              backgroundColor: "#c1fbba",
+              backgroundColor: waitMint ? "transparent" : "#c1fbba",
+              borderColor: waitMint ? "#c1fbba" : "transparent",
+              borderWidth: waitMint ? "2px" : "0",
+              "&.Mui-disabled": {
+                backgroundColor: "rgba(1,1,1,0)",
+                borderStyle: "solid",
+                borderColor: quoteTokenAmount <= 0 ? "rgba(51, 51, 51, 0.1)" : "transparent",
+                borderWidth: quoteTokenAmount <= 0 ? "2px" : "0",
+              },
               color: "#006f16",
               textTransform: "none",
               fontSize: "20px",
               lineHeight: 1,
               height: "42px"
             }}
+            loading={waitMint}
             onClick={handleMint}
           >Mint Pool</Button>
         )}
